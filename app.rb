ca5bd5703ce6004c5a7e1dbe12d341d0862c4d4c
Ryan Stong <rvanstong@gmail.com>
2018-01-02 13:23:19 -0800

    makes views and routes pull from board_config.yaml, in order to provide static info that can be displayed on the board

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require 'sinatra/base'
-require 'erb'
+require 'yaml'
 
 class App < Sinatra::Base
+  before do
+    board_config = YAML::load_file('board_config.yaml')
+    @place_name = board_config['place_name']
+    @intro_message = board_config['intro_message']
+    @lead = board_config['lead']
+    @announcements = board_config['announcements']
+  end
+
   get '/' do
     erb :index
   end
 
   get '/edit' do
+    @slides = [1,2,3]
+    @announcements = [1,2,3]
     erb :edit
   end
 end
