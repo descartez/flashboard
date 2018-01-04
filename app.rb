ea9fda117290fd7aa91579082fe96aa029bf2783
Ryan Stong <rvanstong@gmail.com>
2018-01-04 12:04:34 -0800

    announcements now updates without extra whitespace or \n

--- app.rb
+++ app.rb
@@ -5,8 +5,7 @@ class App < Sinatra::Base
   before do
     @board_config = YAML::load_file('board_config.yaml')
     @place_name = @board_config['place_name']
-    @intro_message = @board_config['place_lead']
-    @lead = @board_config['lead']
+    @intro_message = @board_config['intro_message']
     @announcements = @board_config['announcements']
   end
 
@@ -19,8 +18,16 @@ class App < Sinatra::Base
   end
 
   post '/update' do
+    p params[:announcements].split("-")
     @board_config['place_name'] = params[:place_name]
-    @board_config['place_lead'] = params[:place_lead]
+    @board_config['intro_message'] = params[:intro_message]
+
+    @board_config['announcements'] = params[:announcements].split("-")
+    @board_config['announcements'].each do |announcement|
+      announcement.strip!
+      announcement.sub(/\n/,"")
+    end
+
     File.open('board_config.yaml', 'w') {|f| f.write @board_config.to_yaml }
 
     redirect '/'
--- board_parser.rb
+++ board_parser.rb
@@ -1,18 +1,16 @@
 require 'yaml'
 
 module BoardParser
-  def load(file)
+  def self.load(file)
     @board_config = YAML::load_file(file)
     @place_name = @board_config['place_name']
     @intro_message = @board_config['place_lead']
-    @lead = @board_config['lead']
     @announcements = @board_config['announcements']
   end
 
-  def save(params, file)
+  def self.save(params, file)
     @place_name = params.fetch(:place_name)
     @intro_message = params.fetch(:place_lead)
-    @lead = params.fetch(:lead)
     @announcements = params.fetch(:announcements)
 
     File.open('board_config.yaml', 'w') {|f| f.write @board_config.to_yaml }
