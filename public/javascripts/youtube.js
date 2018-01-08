5c1a50c8b25b26691fd0ae6f80fb983e4e4a05db
Ryan Stong <rvanstong@gmail.com>
2018-01-08 08:36:54 -0800

    youtube frame now loads, autoplays muted, and loops

---
+++ public/javascripts/youtube.js
@@ -0,0 +1,28 @@
+function onYouTubeIframeAPIReady() {
+  console.log('loaded and launched youtube script')
+  var player;
+  player = new YT.Player('youtube_video', {
+    videoId: 'p8VcXbt08mU',
+    width: 560,
+    height: 316,
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      showinfo: 0,
+      modestbranding: 1,
+      loop: 1,
+      fs: 0,
+      cc_load_policy: 0,
+      iv_load_policy: 3,
+      autohide: 0,
+      playlist: 'p8VcXbt08mU'
+    },
+    events: {
+      onReady: function(e) {
+        e.target.mute();
+      }
+    }
+  });
+}
+
+onYouTubeIframeAPIReady()
