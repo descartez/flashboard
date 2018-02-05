d3d6bc91668cf9b815404a6cb92c5c1e45f75deb
Ryan Stong <rvanstong@gmail.com>
2018-02-05 12:47:47 -0800

    gallery now loads and cycles through, need to implement styling to set images at correct sizes

---
+++ public/javascripts/gallery.js
@@ -0,0 +1,12 @@
+console.log('gallery.js is loaded')
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 4000);
