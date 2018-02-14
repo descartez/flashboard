1fae117886ca3d59ebaa66f21082b624c315064f
Ryan Stong <rvanstong@gmail.com>
2018-02-13 19:14:09 -0800

    adds in startup pi script

---
+++ startup-pi.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo '----------------------------------------------------------------------'
+echo 'Welcome to Flashboard by descartez!'
+echo 'This script should be run when you wish to start from a Raspberry Pi.'
+echo '----------------------------------------------------------------------'
+
+source ~/.rvm/scripts/rvm
+
+rvm use 2.4.1
+
+sleep 1s
+echo 'bundling...'
+bundle
+
+sleep 1s
+
+echo 'starting...'
+bundle exec shotgun -o 0.0.0.0 -p 9393 & (sleep 15 && DISPLAY=:0 chromium-browser -kiosk http://localhost:9393)
